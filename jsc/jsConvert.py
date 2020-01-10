69e3061367cbc66d6df9f8322986ef191d336b90
Pmx990 <junzhe.liang@Bellevuecollege.edu>
2020-01-09 16:10:15 -0800

    Add files via upload

--- jsc/jsConvert.py
+++ jsc/jsConvert.py
@@ -2,20 +2,23 @@ import json;
 import requests;
 import operator;
 
+#Hardcoded Json Strings 
 test = '{"classes":[{ "class":"CS100"},{ "class":"ENG100"}, { "class":"CS200"}, { "class":"CS300"}, { "class":"DMA100"}  ]}'    
-
 req = '{"classes":[{ "class":"CS100"},{ "class":"CS200"},{ "class":"CS300"}, { "class":"CS400"}  ]}'
 
+#Get dict from Json
 d1 = json.loads(test);
 d2 = json.loads(req);
 
-#print (d1['classes']);
+
+#Get list from dict
 d1_list = d1['classes']
 d2_list = d2['classes']
 
-#print(d1_list)
 
+#This three methods are recieving List type
 
+#Classes that are taken and in the program
 def same(d1_list,d2_list):
     sameList = [];
     for i in d1_list:
@@ -24,6 +27,7 @@ def same(d1_list,d2_list):
                 sameList.append(i);
     print(sameList)
 
+#Classes that are taken but not required by the program
 def noInProgram(d1_list,d2_list):
     noInList = [];
     for i in d1_list:
@@ -31,6 +35,7 @@ def noInProgram(d1_list,d2_list):
             noInList.append(i);
     print(noInList)
 
+#Classes that are required by the program but not yet taken
 def notTaken(d1_list,d2_list):
     notTakeList = [];
     for i in d2_list:
